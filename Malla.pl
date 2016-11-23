cdf1c9a0e62abd33c3db1b991371a1b55bc183c8
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2016-11-22 21:45:31 -0300

    agregados comentarios

--- Malla.pl
+++ Malla.pl
@@ -1,6 +1,8 @@
+% Primer año
 prerrequisitos(progra).
 prerrequisitos(mate1).
 prerrequisitos(fis100).
+% Otros prerrequisitos
 prerrequisitos(progra, edd).
 prerrequisitos(edd, lp).
 prerrequisitos(progra, ed).
@@ -11,7 +13,7 @@ prerrequisitos(talf, feria).
 prerrequisitos(ed, feria).
 prerrequisitos(lp, feria).
 prerrequisitos(fis100, feria).
-
+% Semestre tentativo para cada ramo
 nsemestre(progra, 1).
 nsemestre(mate1, 1).
 nsemestre(fis100, 1).
@@ -21,26 +23,58 @@ nsemestre(lp, 3).
 nsemestre(talf, 3).
 nsemestre(feria, 4).
 
+% Comprueba si el ramo es requisito directo
 req(X, Z):- prerrequisitos(X, Z).
+
+% Comprueba si el ramo es requisito indirecto
 req(X, Z):- prerrequisitos(X, Y), req(Y, Z).
+
+% Busca todos los resultados entregados por req
 listareq(Cursar, L):- findall(X, req(X, Cursar), L).
 
+% Trabaja la lista como conjunto, eliminando elementos repetidos
+% Caso base
 set([], []).
+
+% Paso recursivo.
 set([H|T], [H|T1]) :-
     remove(H, T, T2),
     set(T2, T1), !.
+
+% Remueve el elemento correspondiente de una lista
+% Caso base, con lista vacia
 remove(_, [], []).
+
+% Remueve de la lista que comienza con el elemento deseado
 remove(X, [X|T], T1) :- remove(X, T, T1).
+
+% Remueve recursivamente en caso contrario
 remove(X, [H|T], [H|T1]) :-
     X \= H,
     remove(X, T, T1).
 
-insert_sort(List,Sorted):-i_sort(List,[],Sorted).
+% Hace un sort sobre la lista
+sortear(List,Sorted):-i_sort(List,[],Sorted).
+
+% Sortea siguiendo insert sort
+% Caso base, con lista vacia
 i_sort([],Acc,Acc):- !.
+
+% Paso recursivo, segun insert
 i_sort([H|T],Acc,Sorted):- insert(H,Acc,NAcc),i_sort(T,NAcc,Sorted), !.
 
+% Inserta segun el semestre correspondiente al ramo
+% Caso primer elemento tiene semestre mayor al segundo
 insert(X,[Y|T],[Y|NT]):- nsemestre(X, A), nsemestre(Y, B), A>B,insert(X,T,NT).
+
+% Caso primer elemento tiene semestre menor o igual al segundo
 insert(X,[Y|T],[X,Y|T]):- nsemestre(X, A), nsemestre(Y, B), A=<B.
+
+% Caso insertar sobre lista vacia
 insert(X,[],[X]).
 
-rama(Aprobados, Cursar, L):- listareq(Cursar, Lista), set(Lista, L1), insert_sort(L1, L2), subtract(L2, Aprobados, L).
+% Dada una lista de Aprobados y un ramo que se desea Cursar, obtiene una
+% lista L con todos los ramos que se deben tomar, de forma ordenada
+% eg: Aprobados = [mate1, fis100, ...]
+%     Cursar = feria
+rama(Aprobados, Cursar, L):- listareq(Cursar, Lista), set(Lista, L1), sortear(L1, L2), subtract(L2, Aprobados, L).
